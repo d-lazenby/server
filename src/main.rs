e94c3f33d436df7c7d2d477ad022a2b55b5b58ee
Doug <douglazenby@gmail.com>
2024-11-10 07:40:15 +0000

    Adding  TCP socket

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod server;
 
 fn main() {
     
-    let server = Server::new("127.0.0:8080".to_string());
+    let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 
 pub struct Server {
@@ -15,6 +16,22 @@ impl Server {
     pub fn run(self) {
         println!("Listening on {}", self.addr);
 
-        let listener = TcpListener::bind(&self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    // if stream > 1024 Bytes, this would fail but this is okay for now.
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
     }
 }
