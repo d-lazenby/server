94552041ac3f1b4cd750b0e7cc9e84d4b4502843
Doug <douglazenby@gmail.com>
2024-11-12 10:25:22 +0000

    Adding request error handling

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 
 pub struct Server {
@@ -21,12 +24,17 @@ impl Server {
         loop {
             match listener.accept() {
                 Ok((mut stream, _)) => {
-                    // if stream > 1024 Bytes, this would fail but this is okay for now.
+                    // if stream > 1024 Bytes, this would fail but 1024 is enough for us in this case.
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                        }
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(_) => {},
+                                Err(e) => println!("Failed to parse the request: {}", e),
+                            }
+                        },
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
                 },
